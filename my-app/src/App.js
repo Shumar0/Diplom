752d3a6a280138cd7fff6d482574993829de5ab9
ilyushik <kamaraliilya@gmail.com>
2025-05-05 17:16:34 +0200

    Main page adopted to React
    Hello World

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+// import Catalog from "./pages/Catalog";
+// import Favorites from "./pages/Favorites";
+// import Account from "./pages/Account";
+// import Product from "./pages/Product";
+// import Cart from "./pages/Cart";
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          {/*<Route path="/catalog" element={<Catalog />} />*/}
+          {/*<Route path="/favorites" element={<Favorites />} />*/}
+          {/*<Route path="/account" element={<Account />} />*/}
+          {/*<Route path="/product" element={<Product />} />*/}
+          {/*<Route path="/cart" element={<Cart />} />*/}
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
